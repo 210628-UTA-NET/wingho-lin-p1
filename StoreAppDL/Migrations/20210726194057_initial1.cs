5123ccef9bd2c1ec67da92ee7b1fbd466a625d25
WingHLin <winghlin1998@gmail.com>
2021-07-27 01:32:16 -0600

    Code first database migrations

---
+++ StoreAppDL/Migrations/20210726194057_initial1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreAppDL.Migrations
+{
+    public partial class initial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LineItems_Customers_CustomerID",
+                table: "LineItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LineItems_CustomerID",
+                table: "LineItems");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerID",
+                table: "LineItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerID",
+                table: "LineItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineItems_CustomerID",
+                table: "LineItems",
+                column: "CustomerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LineItems_Customers_CustomerID",
+                table: "LineItems",
+                column: "CustomerID",
+                principalTable: "Customers",
+                principalColumn: "CustomerID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
