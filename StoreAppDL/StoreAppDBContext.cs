0d4e9b7ed288e0777d82290e37e23c7ac7066ed8
WingHLin <winghlin1998@gmail.com>
2021-07-27 01:28:51 -0600

    Removed OnConfiguring to ignored file

--- StoreAppDL/StoreAppDBContext.cs
+++ StoreAppDL/StoreAppDBContext.cs
@@ -21,15 +21,39 @@ namespace StoreAppDL
         public DbSet<LineItem> LineItems { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Manager> Managers { get; set; }
+        public DbSet<StoreFront> StoreFronts { get; set; }
 
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder p_options)
+        {
+            p_options.UseSqlServer("Server=tcp:wing-lin.database.windows.net,1433;Initial Catalog=WingDB;Persist Security Info=False;User ID=linwing84;Password=Monster7low;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+        */
+        
         protected override void OnModelCreating(ModelBuilder p_modelBuilder)
         {
             p_modelBuilder.Entity<Customer>()
-                .Property(cust => cust.ID)
+                .Property(cust => cust.CustomerID)
                 .ValueGeneratedOnAdd();
 
             p_modelBuilder.Entity<Order>()
                 .Property(ord => ord.OrderID)
+                .ValueGeneratedOnAdd();
+
+            p_modelBuilder.Entity<LineItem>()
+                .Property(li => li.LineItemID)
+                .ValueGeneratedOnAdd();
+
+            p_modelBuilder.Entity<Product>()
+                .Property(prod => prod.ProductID)
+                .ValueGeneratedOnAdd();
+
+            p_modelBuilder.Entity<StoreFront>()
+                .Property(sf => sf.StoreFrontID)
+                .ValueGeneratedOnAdd();
+
+            p_modelBuilder.Entity<Manager>()
+                .Property(man => man.ManagerID)
                 .ValueGeneratedOnAdd();
         }
     }
